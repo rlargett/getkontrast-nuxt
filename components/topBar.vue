1f078b9c9c9f4a5275408f47579304ceade21b97
Ronald Largett <ronlargett@gmail.com>
2019-08-31 12:49:11 -0400

    create and add topbar component

---
+++ components/topBar.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div
+  div(class="flex h-2 shadow")
+    div(class="w-1/6 bg-red rounded-l")
+    div(class="w-1/6 bg-orange")
+    div(class="w-1/6 bg-yellow")
+    div(class="w-1/6 bg-green")
+    div(class="w-1/6 bg-teal")
+    div(class="w-1/6 bg-blue rounded-r")
+
+  div(class="flex flex-col max-w-xl mx-auto justify-center items-center text-center pt-12 text-xl")
+    h1(class="text-4xl m-0 mb-6")
+      span(class="inline-flex justify-center items-center w-24 h-24 mr-1 text-5rem bg-black text-grey-lighter") K
+      span ontrast
+</template>
