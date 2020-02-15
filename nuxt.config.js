e5847fe48c174ab3822a0348c37ebf42bae1e6c1
Ron Largett <ronlargett@gmail.com>
2020-02-14 21:36:41 -0500

    add apple touch icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,8 +13,16 @@ export default {
                 name: 'viewport',
                 content: 'width=device-width, initial-scale=1',
             },
+            {
+                name: 'google-site-verification',
+                content: '0S-5aekwT9PTv7yzEuGvIvHcgGRUNkqHcaZDE5sZbxY',
+            }
         ],
         link: [
+            { rel: 'apple-touch-icon-precomposed', size: '144x144', href: '/apple-touch-icon-144x144.png' },
+            { rel: 'apple-touch-icon-precomposed', size: '114x114', href: '/apple-touch-icon-114x114.png' },
+            { rel: 'apple-touch-icon-precomposed', size: '72x72', href: '/apple-touch-icon-72x72.png' },
+            { rel: 'apple-touch-icon-precomposed', href: '/apple-touch-icon-57x57.png' },
             { rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' },
             { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/hack-font@3/build/web/hack-subset.css' },
         ],
